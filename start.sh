bd45d2bb938fd78b60c3487e191a5ab60d81af02
NIKAM VAIBHAV BHARAT <nikamvaibhav672@gmail.com>
2026-05-27 16:56:29 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Find where pip installed the packages and add to PYTHONPATH
-SITE=$(python3 -c "import site; print(site.getsitepackages()[0])")
-export PYTHONPATH="$SITE:$PYTHONPATH"
-echo "Using PYTHONPATH: $PYTHONPATH"
-python3 bot.py
